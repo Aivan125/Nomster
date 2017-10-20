3957beedf523d37825ff631b568b9d26d8917f88
Ivan Bustos <ivanf.bustos@gmail.com>
2017-10-20 01:54:08 +0000

    Comments in database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments  
+  
   validates :name, length: {minimum: 4}, presence: :true
   validates :address, :description, presence: :true
+  
   geocoded_by :address
   after_validation :geocode 
 end
